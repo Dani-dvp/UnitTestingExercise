031fb28d7c8d0c90f7b367d41cfd34fb2e6799e6
Dani-dvp <kevin1995dani@gmail.com>
2021-11-10 13:14:57 +0100

    first functions without content

---
+++ RobinSkaCrasha/StringHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace RobinSkaCrasha
+{
+    public class StringHelper
+    {
+        public string GetWord(string text, int x, char seperator = ' ')
+        {
+            return string.Empty;
+        }
+
+        public List<string> StringToList(string text, char seperator = ' ')
+        {
+            var textString = new List<string>();
+            return textString;
+        }
+
+        public string RemoveWord(string text, string remove)
+        {
+            return string.Empty;
+        }
+
+        public string RemoveWordAt(string text, int pos)
+        {
+            return string.Empty;
+        }
+
+        public string InsertWordAfter(string text, string after, string add)
+        {
+            return string.Empty;
+        }
+
+        public string SwapWords(string text, int word1, int word2)
+        {
+            return string.Empty;
+        }
+    }
+}
